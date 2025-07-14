482142abdee23126d76939dbfc0ea01b038c7735
Umair Yaquoob <umz78@hotmail.co.uk>
2025-07-14 17:12:40 +0100

    Deploying an internet gateway and associate it with a VPC. Setup a route table with a route to IGW

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Steps for implementation: 
 
 1. [DONE] Deploy a VPC and a subnet 
-2. Deploy an internet gateway and associate it with a the VPC
-3. Setup a route table with a route to the IGW 
+2. [DONE] Deploy an internet gateway and associate it with a the VPC
+3. [DONE] Setup a route table with a route to the IGW 
 4. Deploy an EC2 instance inside of the created subnet 
 5. Associate a public IP and security group that allows public ingress
 
--- networking.tf
+++ networking.tf
@@ -18,3 +18,34 @@ resource "aws_subnet" "public" {
     Project   = "ec2-vpc"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "ec2-vpc"
+    ManagedBy = "Terraform"
+    Project   = "ec2-vpc"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/10"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name      = "ec2-vpc"
+    ManagedBy = "Terraform"
+    Project   = "ec2-vpc"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
