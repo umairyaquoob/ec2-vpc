dbd8526d86c827ad596fbac79b9b47cca5d17d09
Umair Yaquoob <umz78@hotmail.co.uk>
2025-07-22 17:06:27 +0100

    Associate a security group that allows public ingress

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Steps for implementation:
 1. [DONE] Deploy a VPC and a subnet 
 2. [DONE] Deploy an internet gateway and associate it with a the VPC
 3. [DONE] Setup a route table with a route to the IGW and assocaite with a subnet
-4. [DONE] Deploy an EC2 instance inside of the created subnet 
-5. Associate a public IP and security group that allows public ingress
+4. [DONE] Deploy an EC2 instance inside of the created subnet and associate a public IP 
+5. [DONE] Associate a security group that allows public ingress
 6. Change the EC2 instance to use a publicity available NGINX AMI
 7. Destory everything
 
--- conmpute.tf
+++ conmpute.tf
@@ -1,12 +1,45 @@
-#resource "aws_instance" "web" {
-#  ami                         = "ami-064673ca419016c37"
-#  associate_public_ip_address = true
-#  instance_type               = "t2.micro"
-#  subnet_id                   = aws_subnet.public.id
-#  root_block_device {
-#    delete_on_termination = true
-#    volume_size           = 10
-#    volume_type           = "gp3"
-#  }
+resource "aws_instance" "web" {
+  ami                         = "ami-064673ca419016c37"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
 
-#}
+  tags = merge(local.common_tags, {
+    Name = "ec2"
+  })
+
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "sg"
+  })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+
+}
